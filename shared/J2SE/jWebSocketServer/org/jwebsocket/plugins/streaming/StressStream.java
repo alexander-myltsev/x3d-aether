31af1239729e27f5943f9eeca6c91af95e30cb5f
Alexander Myltsev <alexander.myltsev@gmail.com>
2011-01-08 20:52:43 +0500

    Some o3d (http://code.google.com/p/o3d/) were added. And tests with jWebSocket StressStrem are done.

--- shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/streaming/StressStream.java
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/streaming/StressStream.java
@@ -24,12 +24,12 @@ import org.jwebsocket.server.TokenServer;
 import org.jwebsocket.token.Token;
 
 /**
- * implements the stressStream, primarily for demonstration purposes but it can
- * also be used for client/server stress synchronization. It implements an
- * internal thread which broadcasts the current system stress of the server to
- * the registered clients once per second.
- * @author aschulze
- */
+* implements the stressStream, primarily for demonstration purposes but it can
+* also be used for client/server stress synchronization. It implements an
+* internal thread which broadcasts the current system stress of the server to
+* the registered clients once per second.
+* @author aschulze
+*/
 public class StressStream extends TokenStream {
 
 	private static Logger log = Logging.getLogger(StressStream.class);
@@ -105,7 +105,7 @@ public class StressStream extends TokenStream {
 					Token lToken = new Token("event");
 					lToken.put("name", "stream");
 					lToken.put("msg", new Date().getTime());
-					//lToken.put("seconds", Calendar.getInstance().get(Calendar.SECOND));					
+					lToken.put("seconds", Calendar.getInstance().get(Calendar.SECOND));					
 
 					put(lToken);
 				} catch (InterruptedException ex) {
@@ -118,3 +118,99 @@ public class StressStream extends TokenStream {
 		}
 	}
 }
+
+///**
+// * implements the stressStream, primarily for demonstration purposes but it can
+// * also be used for client/server stress synchronization. It implements an
+// * internal thread which broadcasts the current system stress of the server to
+// * the registered clients once per second.
+// * @author aschulze
+// */
+//public class StressStream extends TokenStream {
+//
+//	private static Logger log = Logging.getLogger(StressStream.class);
+//	private Boolean isRunning = false;
+//	private StressProcess stressProcess = null;
+//	private Thread stressThread = null;
+//
+//	/**
+//	 *
+//	 *
+//	 * @param aStreamID
+//	 * @param aServer
+//	 */
+//	public StressStream(String aStreamID, TokenServer aServer) {
+//		super(aStreamID, aServer);
+//		startStream(-1);
+//	}
+//
+//	/**
+//	 *
+//	 */
+//	@Override
+//	public void startStream(long aTimeout) {
+//		super.startStream(aTimeout);
+//
+//		if (log.isDebugEnabled()) {
+//			log.debug("Starting stress stream...");
+//		}
+//		stressProcess = new StressProcess();
+//		stressThread = new Thread(stressProcess);
+//		stressThread.start();
+//	}
+//
+//	/**
+//	 *
+//	 */
+//	@Override
+//	public void stopStream(long aTimeout) {
+//		if (log.isDebugEnabled()) {
+//			log.debug("Stopping stress stream...");
+//		}
+//		long lStarted = new Date().getTime();
+//		isRunning = false;
+//		try {
+//			stressThread.join(aTimeout);
+//		} catch (Exception ex) {
+//			log.error(ex.getClass().getSimpleName() + ": " + ex.getMessage());
+//		}
+//		if (log.isDebugEnabled()) {
+//			long lDuration = new Date().getTime() - lStarted;
+//			if (stressThread.isAlive()) {
+//				log.warn("stress stream did not stopped after " + lDuration + "ms.");
+//			} else {
+//				log.debug("stress stream stopped after " + lDuration + "ms.");
+//			}
+//		}
+//
+//		super.stopStream(aTimeout);
+//	}
+//
+//	private class StressProcess implements Runnable {
+//
+//		@Override
+//		public void run() {
+//			if (log.isDebugEnabled()) {
+//				log.debug("Running stress stream...");
+//			}
+//			isRunning = true;
+//			while (isRunning) {
+//				try {
+//					Thread.sleep(50);
+//
+//					Token lToken = new Token("event");
+//					lToken.put("name", "stream");
+//					lToken.put("msg", new Date().getTime());
+//					lToken.put("seconds", Calendar.getInstance().get(Calendar.SECOND));					
+//
+//					put(lToken);
+//				} catch (InterruptedException ex) {
+//					log.error("(run) " + ex.getClass().getSimpleName() + ": " + ex.getMessage());
+//				}
+//			}
+//			if (log.isDebugEnabled()) {
+//				log.debug("stress stream stopped.");
+//			}
+//		}
+//	}
+//}
